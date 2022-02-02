c3c845d2d3a38d244c850082d8c9031ea1216ed6
Abhishek  R Prasad <97088891+Abhishek32971@users.noreply.github.com>
2022-02-02 03:24:56 +0000

    commit

--- p1original.c
+++ p1original.c
@@ -1,2 +1,21 @@
-
+#include<stdio.h>
+void input(int *a,int *b)
+{
+  printf("enter the values of a,b\n");
+  scanf("%d %d",a,b);
+}
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("sum of %d+%d is %d\n",a,b,sum);
+}
+void main()
+{
+  int a,b,s;
+  input(&a,&b);
+  add(a,b,&s);
+  output(a,b,s);
 }
